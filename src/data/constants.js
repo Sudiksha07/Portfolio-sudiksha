4a3697521eab559d8e15832f509e93f93f58b795
Sudiksha Gandhi <sudiksha.gandhi07@gmailcom>
2024-07-01 22:17:45 +0530

    changes

--- src/data/constants.js
+++ src/data/constants.js
@@ -14,7 +14,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Sudiksha07/Portfolio",
   resume:
-" ",
+" https://drive.google.com/file/d/10zMrhxU-WynU6L5q2_jbHcnryKmeRpri/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/sudiksha-gandhi-4a9387237/",
   Github: "https://github.com/Sudiksha07/Portfolio",
   insta: "https://www.instagram.com/",
